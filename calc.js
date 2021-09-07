bcf70e108b9d6d36baa4a3b5777627151d468488
antoRados <antorados@gmail.com>
2021-09-07 16:06:39 -0400

    Addition implementation added

--- calc.js
+++ calc.js
@@ -5,16 +5,39 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-
-
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	
+	
+	// addition
+	if (mathSymbol === "+") {
+		console.log(num1 + num2);
+	}
+	
+	// subtraction
+	else if (mathSymbol === "-") {
+		console.log(num1 - num2);
+	}
+	
+	// multiplication
+	else if (mathSymbol === "*") {
+		console.log(num1 + num2);
+	}
 
+	// division
+	else if (mathSymbol === "/") {
+		console.log(num1 + num2);
+	}
 
+	// square root
+	else if (mathSymbol === "√") {
+		console.log(Math.sqrt(num1));
+	}
+	
 	// This line closes the connection to the command line interface.
 	reader.close()
 
