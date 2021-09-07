f9f32ae49aafd120bb75d1ea3b6b3d8a5a2da30f
antoRados <antorados@gmail.com>
2021-09-07 16:10:10 -0400

    Square root implementation added

--- calc.js
+++ calc.js
@@ -33,6 +33,11 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 + num2);
 	}
 	
+	// square root
+	else if (mathSymbol === "√") {
+		console.log(Math.sqrt(num1));
+	}
+	
 	// This line closes the connection to the command line interface.
 	reader.close()
 
